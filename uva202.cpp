8d311b9e69ef09e8dc5e1322558197497b42a26e
Zhicheng Chen <chen1319276472@foxmail.com>
2018-05-22 01:25:06 +0800

    180521

---
+++ uva202.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+// 是时候展现真正的技术了(手动斜眼)
+// 蜜汁PE
+int main(){
+    int ans[30005];
+    int vis[30005];
+    int a, b;
+    int op, ed;
+    int aa, bb;
+    while(cin>>a>>b){
+        aa = a;
+        bb = b;
+        memset(vis, -1, sizeof(vis));
+        memset(ans, 0, sizeof(ans));
+        for(int i=0;i<3000;i++, a %= b, a *= 10){
+            ans[i] = a / b;
+            if(i != 0){
+                if(vis[a] == -1){
+                    vis[a] = i;
+                } else {
+                    op = vis[a];
+                    ed = i;
+                    break;
+                }
+            }
+        }
+        cout<<aa<<"/"<<bb<<" = "<<ans[0]<<".";
+        for(int i = 1; i < ed && i < op + 50; i++){
+            if(i == op) cout<<"(";
+            cout<<ans[i];
+        }
+        if(ed - op > 50) cout<<"...";
+        cout<<")"<<endl;
+        cout<<"   "<<ed - op<<" = number of digits in repeating cycle"<<endl;
+    }
+    return 0;
+}
